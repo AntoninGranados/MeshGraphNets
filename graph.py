Minor fixes in the miniellipse algorithm (fixed a possible error from matrix inverse computation)

graph.py
@@ -20,6 +20,7 @@ class NodeType(IntEnum):
     # WALL_BOUNDARY = 6
     COUNT = 9
 
+# TODO: change edges shape from (2, ...) to (..., 2)
 def cells_to_edges(mesh: Mesh) -> torch.Tensor:
     """ Compute the graph edges from the cells (triangles) """
 
@@ -45,11 +46,12 @@ def cells_to_edges(mesh: Mesh) -> torch.Tensor:
         ], dim=-1
     )
 
-# TODO: implement for batch size > 1
+# TODO: handle batch size > 1
 def compute_world_edges(mesh: Mesh, meta: dict, edges: torch.Tensor|None = None) -> torch.Tensor:
     if edges is None:
         edges = cells_to_edges(mesh).rot90()
 
+    #! torch.cdist might work here ?
     neighbours = radius(mesh["world_pos"][0], mesh["world_pos"][0], r=meta["collision_radius"]).rot90()
 
     edge_mask = ~(edges[:,None]==neighbours).all(dim=-1).any(dim=0)

miniellipse.py
@@ -14,7 +14,10 @@ def ellipse_through(points):
 		p1 = np.array(points[0])
 		p2 = np.array(points[1])
 		A = np.outer(p1, p1) + np.outer(p2, p2)
-		return np.linalg.inv(A)
+		try:
+			return np.linalg.inv(A)
+		except:
+			return np.zeros((2,2))
 
 	if len(points) == 3:
 		A = np.array([