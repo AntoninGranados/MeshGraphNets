e1868e6a50c1a8c7d802f51621d37d2c9ebd4169
Antonin Granados <antonin.granados@gmail.com>
2025-09-18 13:55:17 +0200

    Implemented the split function (1st step of the remeshing algorithm)

--- test.py
+++ test.py
@@ -12,15 +12,16 @@ import matplotlib.tri as mtri
 from matplotlib.patches import Ellipse as PltEllipse
 from pathlib import Path
 from collections import namedtuple
+from typing import Any
 
-def plot_mesh(ax, mesh: Mesh):
+def plot_mesh(ax, mesh: Mesh, color="w"):
     triangles = mesh["cells"][BATCH]
     type_mask = (mesh["node_type"][BATCH][triangles[:,0]]==NodeType.NORMAL).any(dim=-1).flatten()
     type_mask |= (mesh["node_type"][BATCH][triangles[:,1]]==NodeType.NORMAL).any(dim=-1).flatten()
     type_mask |= (mesh["node_type"][BATCH][triangles[:,2]]==NodeType.NORMAL).any(dim=-1).flatten()
 
     triang = mtri.Triangulation(mesh["mesh_pos"][BATCH][:,0], mesh["mesh_pos"][BATCH][:,1], triangles, ~type_mask)
-    ax.triplot(triang, linewidth=1, color="w")
+    ax.triplot(triang, linewidth=1, color=color)
     ax.set_aspect('equal')
     ax.set_ylim(1, 0)
     ax.set_axis_off()
@@ -38,206 +39,124 @@ def plot_ellipse(ax, S: torch.Tensor, pos=(0,0), c="r") -> None:
 
 
 EPS = 1e-3
+AR_THRESHOLD = 1e3 # aspect ratio threshold
 
 MeshState = namedtuple(
     "MeshState",
     ["edges", "opposites", "node_mask", "mesh_pos", "world_pos", "S_i"]
 )
 
-def splittable_edges(ms: MeshState) -> torch.Tensor:
-    normal_edge_mask = ms.node_mask[ms.edges[:,0]] | ms.node_mask[ms.edges[:,1]]
+def get_splittable_edges_mask(ms: MeshState) -> torch.Tensor:
+    """ Find splittable edges """
+    S_ij = 0.5 * (ms.S_i[ms.edges[:, 0]] + ms.S_i[ms.edges[:, 1]])
+    u_ij = ms.mesh_pos[ms.edges[:, 1]] - ms.mesh_pos[ms.edges[:, 0]]
+    edge_size = torch.einsum("ei,eij,ej->e", u_ij, S_ij, u_ij)
+    return (edge_size > 1 + EPS)
 
-    S_ij = 0.5 * (ms.S_i[ms.edges[:,0]] + ms.S_i[ms.edges[:,1]])
-    u_ij = ms.mesh_pos[ms.edges[:,0]] - ms.mesh_pos[ms.edges[:,1]]
+def get_maximal_independent_set(ms: MeshState, mask: torch.Tensor) -> torch.Tensor:
+    """ Find a maximal independent set """
 
-    metric = torch.einsum("ei,eij,ej->e", u_ij, S_ij, u_ij)
-    return (metric > 1 + EPS) & normal_edge_mask
+    maximal_independent_set = []
+    node_set = set()
+    for e_id in torch.nonzero(mask):
+        if ms.edges[e_id,0].item() not in node_set and ms.edges[e_id, 1].item() not in node_set:
+            maximal_independent_set.append(e_id)
+            node_set.add(ms.edges[e_id, 0].item())
+            node_set.add(ms.edges[e_id, 1].item())
 
-def maximal_independent_set(edges: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-    eligible_indices = torch.nonzero(mask).squeeze()
-    independent_set = []
-    used_vertices = set()
-    
-    for idx in eligible_indices:
-        i, j = edges[idx].tolist()
-        if i not in used_vertices and j not in used_vertices:
-            independent_set.append(idx.item())
-            used_vertices.add(i)
-            used_vertices.add(j)
-    
-    return torch.Tensor(independent_set).type(torch.long)
+    return torch.Tensor(maximal_independent_set).type(torch.long)
 
-def split_edge_set(ms: MeshState, independent_set: torch.Tensor) -> MeshState:
-    edges_to_split = ms.edges[independent_set]
+# TODO: add a stopping contidition (max_iter ?)
+# TODO: flip edge at the end of each iteration
+def split(ms: MeshState) -> MeshState:
+    """ Split all possible edges in `ms` """
 
-    # interpolate values
-    new_mesh_pos = torch.concat([
-        ms.mesh_pos,
-        0.5 * (ms.mesh_pos[edges_to_split[:,0]] + ms.mesh_pos[edges_to_split[:,1]])
-    ], dim=0)
-    
-    new_world_pos = torch.concat([
-        ms.world_pos,
-        0.5 * (ms.world_pos[edges_to_split[:,0]] + ms.world_pos[edges_to_split[:,1]])
-    ], dim=0)
-
-    interpolated_S_i = 0.5 * (ms.S_i[edges_to_split[:,0]] + ms.S_i[edges_to_split[:,1]])
-    interpolated_S_i = torch.stack([closest_SDP(e) for e in interpolated_S_i.unbind(dim=0)], dim=0)
-    new_S_i = torch.concat([ms.S_i, interpolated_S_i], dim=0)
-
-    new_edges = ms.edges.clone()
-    new_opposites = ms.opposites.clone()
-    new_node_mask = ms.node_mask.clone()
-    for e_id in independent_set:
-        i, j = ms.edges[e_id].unbind()
-        k, l = ms.opposites[e_id].unbind()
-        m = len(new_node_mask)
-
-        new_node_mask = torch.concat([new_node_mask, torch.Tensor([True])]).type(torch.bool)
-        
-        # remove the splitted edge
-        new_edges = torch.concat([new_edges[:e_id], new_edges[e_id+1:]], dim=0)
-        new_opposites = torch.concat([new_opposites[:e_id], new_opposites[e_id+1:]], dim=0)
-
-        # compute new edges
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, i]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[k, l]])], dim=0)
-
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, j]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[k, l]])], dim=0)
-
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, k]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[i, j]])], dim=0)
-
-        if l != -1:   # l = -1 if the edge is on the border and have only one "opposite" point
-            new_edges = torch.concat([new_edges, torch.Tensor([[m, l]])], dim=0)
-            new_opposites = torch.concat([new_opposites, torch.Tensor([[i, j]])], dim=0)
-
-        # compute new opposites
-        ik = find_edge(i, k, new_edges)
-        new_opposites[ik][new_opposites[ik] == j] = m
-
-        jk = find_edge(j, k, new_edges)
-        new_opposites[jk][new_opposites[jk] == i] = m
-
-        if l != -1:
-            il = find_edge(i, l, new_edges)
-            new_opposites[il][new_opposites[il] == j] = m
-
-            jl = find_edge(j, l, new_edges)
-            new_opposites[jl][new_opposites[jl] == i] = m
-
-    return MeshState(
-        edges = new_edges.type(torch.long),
-        opposites = new_opposites.type(torch.long),
-        node_mask = new_node_mask.type(torch.bool),
-        mesh_pos = new_mesh_pos,
-        world_pos = new_world_pos,
-        S_i = new_S_i
-    )
-
-def split_edges(ms: MeshState) -> MeshState:
     while True:
-        splittable_mask = splittable_edges(ms)
-        independent_set = maximal_independent_set(ms.edges, splittable_mask)
-        print(len(independent_set))
-        if len(independent_set) == 0: break
+        edge_mask = ms.node_mask[ms.edges[:, 0]] | ms.node_mask[ms.edges[:, 1]]
+        invalid_edges = get_splittable_edges_mask(ms) & edge_mask
+        maximal_independent_set = get_maximal_independent_set(ms, invalid_edges)
+        if len(maximal_independent_set) == 0: return ms
+
+        # interpolate values
+        i_, j_ = ms.edges[maximal_independent_set][:,0], ms.edges[maximal_independent_set][:,1]
+        m_mesh_pos  = 0.5 * (ms.mesh_pos[i_] + ms.mesh_pos[j_])
+        m_world_pos = 0.5 * (ms.world_pos[i_] + ms.world_pos[j_])
+        m_S_i       = 0.5 * (ms.S_i[i_] + ms.S_i[j_])
+        m_S_i       = torch.stack([closest_SDP(S) for S in torch.unbind(m_S_i, dim=0)], dim=0)
+        new_node_mask = torch.concat([ms.node_mask.clone(), torch.Tensor([True]*len(maximal_independent_set))])
+        new_mesh_pos  = torch.concat([ms.mesh_pos.clone(),  m_mesh_pos])
+        new_world_pos = torch.concat([ms.world_pos.clone(), m_world_pos])
+        new_S_i       = torch.concat([ms.S_i.clone(),       m_S_i])
+
+        # split edges
+        def add_edge(nodes: tuple[Any,Any], opposites: tuple[Any,Any]) -> None:
+            new_edges_buf.append(nodes)
+            new_opposites_buf.append(opposites)
+
+        def correct_opposites(nodes: tuple[Any,Any], old, new) -> None:
+            e_id = find_edge(nodes[0], nodes[1], new_edges)
+            new_opposites[e_id][new_opposites[e_id] == old] = new
+
+        def get_aspect_ratio(i, j, k) -> torch.Tensor:
+            a = torch.norm(new_mesh_pos[i].unsqueeze(0) - new_mesh_pos[j].unsqueeze(0))
+            b = torch.norm(new_mesh_pos[j].unsqueeze(0) - new_mesh_pos[k].unsqueeze(0))
+            c = torch.norm(new_mesh_pos[k].unsqueeze(0) - new_mesh_pos[i].unsqueeze(0))
+
+            return a*b*c / ((b+c-a)*(c+a-b)*(a+b-c))
         
-        print(ms.edges.shape)
-        ms = split_edge_set(ms, independent_set)
-        print(ms.edges.shape)
-        
-        # new_faces = get_faces_from_edges(ms, independent_set)
-        # ms = flip_edges(ms, active_faces=new_faces)
-    
-    return ms
+        def split_edge(i, j, k, l, m) -> None:
+            # check if new triangles would have a bad aspect ratio
+            if get_aspect_ratio(i, m, k) > AR_THRESHOLD or get_aspect_ratio(j, m, k) > AR_THRESHOLD: return
+            if l != -1:
+                if get_aspect_ratio(i, m, l) > AR_THRESHOLD or get_aspect_ratio(j, m, l) > AR_THRESHOLD: return
 
-"""
-def split_edges(ms: MeshState) -> MeshState:
-    normal_edge_mask = ms.node_mask[ms.edges[:,0]] | ms.node_mask[ms.edges[:,1]]
+            # add new edges and their corresponding opposite nodes
+            add_edge((i, m), (k, l))
+            add_edge((j, m), (k, l))
+            add_edge((k, m), (i, j))
+            if l != -1:
+                add_edge((l, m), (i, j))
 
-    # find the edges to split
-    S_ij = 0.5 * (ms.S_i[ms.edges[:,0]] + ms.S_i[ms.edges[:,1]])
-    u_ij = ms.mesh_pos[ms.edges[:,0]] - ms.mesh_pos[ms.edges[:,1]]
+            # correct opposite nodes of adjacent edges
+            correct_opposites((i, k), j, m)
+            correct_opposites((j, k), i, m)
+            if l != -1:
+                correct_opposites((i, l), j, m)
+                correct_opposites((j, l), i, m)
 
-    metric = torch.einsum("ei,eij,ej->e", u_ij, S_ij, u_ij)
-    metric[~normal_edge_mask] = float("nan")
 
-    invalid_edges_mask = metric > 1 + EPS
-    invalid_edges = ms.edges[invalid_edges_mask]
-    # sort edges by metric
-    argsort_metric = torch.argsort(metric[invalid_edges_mask], descending=True)
+        new_edges = ms.edges.clone()
+        new_opposites = ms.opposites.clone()
+        new_edges_buf = []
+        new_opposites_buf = []
+        for it, e_id in enumerate(maximal_independent_set):
+            i, j = i_[it], j_[it]
+            k, l = new_opposites[e_id].unbind()
+            m = len(ms.node_mask) + it
+            split_edge(i, j, k, l, m)
 
-    # TODO: interpolate S_i when creating `m`
-    new_mesh_pos = torch.concat([
-        ms.mesh_pos,
-        0.5 * (ms.mesh_pos[invalid_edges[:,0]] + ms.mesh_pos[invalid_edges[:,1]])
-    ], dim=0)
-    new_node_mask = torch.concat([ms.node_mask, torch.Tensor([True]*len(invalid_edges))]).type(torch.bool)
-    new_world_pos = torch.concat([
-        ms.world_pos,
-        0.5 * (ms.world_pos[invalid_edges[:,0]] + ms.world_pos[invalid_edges[:,1]])
-    ], dim=0)
+        new_edges     = torch.concat([new_edges, torch.Tensor(new_edges_buf)]).type(torch.long)
+        new_opposites = torch.concat([new_opposites, torch.Tensor(new_opposites_buf)]).type(torch.long)
 
-    interpolated_S_i = 0.5 * (ms.S_i[invalid_edges[:,0]] + ms.S_i[invalid_edges[:,1]])
-    interpolated_S_i = torch.stack([closest_SDP(e) for e in interpolated_S_i.unbind(dim=0)], dim=0)
-    new_S_i = torch.concat([ms.S_i, interpolated_S_i], dim=0)
+        invalid_edges = torch.zeros(len(new_edges)).type(torch.bool)
+        invalid_edges[maximal_independent_set] = True
+        new_edges = new_edges[~invalid_edges]
+        new_opposites = new_opposites[~invalid_edges]
 
-    new_edges = ms.edges
-    new_opposites = ms.opposites
-    for e_id in argsort_metric:
-        i, j = invalid_edges[e_id].unbind()
-        k, l = new_opposites[invalid_edges_mask][e_id].unbind()
-        m = len(ms.node_mask) + e_id # offset in `new_nodes_pos`
-        
-        # compute new edges
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, i]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[k, l]])], dim=0)
-
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, j]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[k, l]])], dim=0)
-
-        new_edges = torch.concat([new_edges, torch.Tensor([[m, k]])], dim=0)
-        new_opposites = torch.concat([new_opposites, torch.Tensor([[i, j]])], dim=0)
-
-        if l != -1:   # l = -1 if the edge is on the border and have only one "opposite" point
-            new_edges = torch.concat([new_edges, torch.Tensor([[m, l]])], dim=0)
-            new_opposites = torch.concat([new_opposites, torch.Tensor([[i, j]])], dim=0)
-            invalid_edges_mask = torch.concat([invalid_edges_mask, torch.Tensor([False])]).type(torch.bool)
-
-        for _ in range(3):
-            invalid_edges_mask = torch.concat([invalid_edges_mask, torch.Tensor([False])]).type(torch.bool)
-        
-
-        # compute new opposites
-        ik = find_edge(i, k, new_edges)
-        new_opposites[ik][new_opposites[ik] == j] = m
-
-        jk = find_edge(j, k, new_edges)
-        new_opposites[jk][new_opposites[jk] == i] = m
-
-        if l != -1:
-            il = find_edge(i, l, new_edges)
-            new_opposites[il][new_opposites[il] == j] = m
-
-            jl = find_edge(j, l, new_edges)
-            new_opposites[jl][new_opposites[jl] == i] = m
-
-    return MeshState(
-        edges = new_edges.type(torch.long),
-        opposites = new_opposites.type(torch.long),
-        node_mask = new_node_mask.type(torch.bool),
-        mesh_pos = new_mesh_pos,
-        world_pos = new_world_pos,
-        S_i = new_S_i
-    )
-"""
+        ms = MeshState(
+            edges = new_edges.type(torch.long),
+            opposites = new_opposites.type(torch.long),
+            node_mask = new_node_mask.type(torch.bool),
+            mesh_pos = new_mesh_pos,
+            world_pos = new_world_pos,
+            S_i = new_S_i
+        )
 
 ds = Dataset(Path("dataset", "sphere_dynamic"), stage="valid")
 
 traj_len = ds.meta["trajectory_length"]
 traj = 0
-time_idx = 35
+time_idx = 40
 
 """
 COMPUTATION
@@ -247,7 +166,6 @@ mesh2 = batch_dicts([ds[traj_len*traj + time_idx + 1]])
 
 normal1_mask = mesh1["node_type"][BATCH].flatten() == NodeType.NORMAL
 normal2_mask = mesh2["node_type"][BATCH].flatten() == NodeType.NORMAL
-
 
 sizing_field2 = get_sizing_field_tensor(mesh2)
 S_i = interpolate_field(mesh1, mesh2, sizing_field2)
@@ -265,91 +183,10 @@ ms = MeshState(
     S_i = S_i
 )
 
-split_ms = split_edges(ms)
+new_ms = split(ms)
+new_edge_count = len(new_ms.edges) - len(ms.edges)  #! wrong computation (does not account for removed edges)
 
-exit(-1)
-
-normal_edge_mask = split_ms.node_mask[split_ms.edges[:,0]] | split_ms.node_mask[split_ms.edges[:,1]]
-border_mask = split_ms.opposites[:,1] == -1
-mask = normal_edge_mask & (~border_mask)
-i = split_ms.edges[:,0]
-j = split_ms.edges[:,1]
-k = split_ms.opposites[:,0]
-l = split_ms.opposites[:,1]
-M_avg = 0.25 * (split_ms.S_i[i] + split_ms.S_i[j] + split_ms.S_i[k] + split_ms.S_i[l])
-
-u_ik = split_ms.mesh_pos[i] - split_ms.mesh_pos[k]
-u_il = split_ms.mesh_pos[i] - split_ms.mesh_pos[l]
-u_jk = split_ms.mesh_pos[j] - split_ms.mesh_pos[k]
-u_jl = split_ms.mesh_pos[j] - split_ms.mesh_pos[l]
-
-ujk_x_uik = u_jk[:, 0] * u_ik[:, 1] - u_jk[:, 1] * u_ik[:, 0]
-uil_x_ujl = u_il[:, 0] * u_jl[:, 1] - u_il[:, 1] * u_jl[:, 0]
-uil_M_ujl = torch.einsum("ei,eij,ej->e", u_il, M_avg, u_jl)
-ujk_M_uik = torch.einsum("ei,eij,ej->e", u_jk, M_avg, u_ik)
-
-delaunay_cond = (ujk_x_uik * uil_M_ujl) + (ujk_M_uik * uil_x_ujl) < -EPS
-delaunay_cond[~mask] = False
-print(torch.count_nonzero(delaunay_cond))
-
-new_edges = split_ms.edges.clone()
-new_opposites = split_ms.opposites.clone()
-for e_id in torch.nonzero(delaunay_cond):
-    i_, j_ = i[e_id].item(), j[e_id].item()
-    # k_, l_ = new_opposites[find_edge(i_, j_, split_ms.edges)].unbind()
-    k_, l_ = k[e_id].item(), l[e_id].item()
-
-    new_edges[e_id] = torch.Tensor([k_, l_]).type(torch.long)
-    new_opposites[e_id] = torch.Tensor([i_, j_]).type(torch.long)
-
-    ik = find_edge(i_, k_, split_ms.edges)
-    new_opposites[ik][new_opposites[ik] == j_] = l_
-
-    jk = find_edge(j_, k_, new_edges)
-    new_opposites[jk][new_opposites[jk] == i_] = l_
-
-    il = find_edge(i_, l_, new_edges)
-    new_opposites[il][new_opposites[il] == j_] = k_
-
-    jl = find_edge(j_, l_, new_edges)
-    new_opposites[jl][new_opposites[jl] == i_] = k_
-
-exit(0)
-
-"""
-flip1_ms = MeshState(
-    edges = new_edges,
-    opposites = new_opposites,
-    node_mask = split_ms.node_mask,
-    mesh_pos = split_ms.mesh_pos,
-    world_pos = split_ms.world_pos,
-    S_i = split_ms.S_i
-)
-"""
-
-"""
-# compute common nodes between the meshes
-nodes_dist = torch.cdist(mesh1["mesh_pos"][0], mesh2["mesh_pos"][0])
-nodes_dist[:,~normal2_mask] = float("inf")
-
-common_nodes = (nodes_dist < EPS).any(dim=1) & normal1_mask
-delete_nodes = ~common_nodes & normal1_mask
-
-# compute node pairs (mesh1_i <-> mesh2_j | nan if not NORMAL or not in mesh2)
-node_pairs = torch.argmin(nodes_dist, dim=1)
-node_pairs = np.where(
-    common_nodes,
-    node_pairs,
-    float("nan"),
-)
-
-# compute the containing triangle for each point
-sorted_cells = mesh2["cells"][BATCH]
-cell_mask = normal2_mask[sorted_cells[:,0]] | normal2_mask[sorted_cells[:,1]] | normal2_mask[sorted_cells[:,2]]
-sorted_cells = sorted_cells[cell_mask]
-tris, bary = find_enclosing_triangle(mesh1["mesh_pos"][BATCH][delete_nodes], mesh2["mesh_pos"][BATCH], sorted_cells)
-containing_tris = sorted_cells[tris]
-"""
+# exit(1)
 
 """
 DISPLAY
@@ -361,52 +198,20 @@ ax1, ax2, ax3, ax4 = fig.add_subplot(141), fig.add_subplot(142), fig.add_subplot
 ax1.set_title("Starting Mesh")
 plot_mesh(ax1, mesh1)
 
-"""
-c = np.where(common_nodes[normal1_mask], "g", "r")
-ax1.scatter(mesh1["mesh_pos"][0,:,0][normal1_mask], mesh1["mesh_pos"][0,:,1][normal1_mask], c=c, alpha=0.8)
-idx = 0
-ax1.scatter(mesh1["mesh_pos"][BATCH][delete_nodes][idx,0],    mesh1["mesh_pos"][BATCH][delete_nodes][idx,1], c="c")
-ax1.scatter(mesh2["mesh_pos"][BATCH][containing_tris[idx],0], mesh2["mesh_pos"][BATCH][containing_tris[idx],1], c="c", marker="x", s=40)
-"""
-
-
-ax2.set_title("Splited Edges")
+ax2.set_title("Splitted Edges")
+edge_mask = new_ms.node_mask[new_ms.edges[:, 0]] | new_ms.node_mask[new_ms.edges[:, 1]]
+for e_id in torch.nonzero(edge_mask):
+    e_id = e_id.item()
+    ax2.plot(new_ms.mesh_pos[new_ms.edges[e_id]][:, 0], new_ms.mesh_pos[new_ms.edges[e_id]][:, 1], "c", linewidth=0.5, zorder=-1)
 plot_mesh(ax2, mesh1)
-for i in range(len(edges), len(split_ms.edges)):
-     ax2.plot(split_ms.mesh_pos[split_ms.edges[i],0], split_ms.mesh_pos[split_ms.edges[i],1], c="r", linewidth=1, alpha=0.6)
 
-
-ax3.set_title("Mesh")
-for e in split_ms.edges[normal_edge_mask]:
-     ax3.plot(split_ms.mesh_pos[e,0], split_ms.mesh_pos[e,1], c="w", linewidth=1)
-# for e in split_ms.edges[mask][delaunay_cond]:
-#      ax3.plot(split_ms.mesh_pos[e,0], split_ms.mesh_pos[e,1], c="r", linewidth=1)
-for e in new_edges[delaunay_cond]:
-     ax3.plot(split_ms.mesh_pos[e,0], split_ms.mesh_pos[e,1], c="r", linewidth=0.5)
-
-ax3.set_aspect('equal')
-ax3.set_ylim(1, 0)
-ax3.set_axis_off()
+ax3.set_title("Collapsed Edges")
+plot_mesh(ax3, mesh1, "gray")
 
 ax4.set_title("Target Mesh")
+plot_mesh(ax4, mesh1, "r")
 plot_mesh(ax4, mesh2)
-for e in split_ms.edges[normal_edge_mask]:
-     ax4.plot(split_ms.mesh_pos[e,0], split_ms.mesh_pos[e,1], c="c", linewidth=1, alpha=0.5)
 
-"""
-# display ellipses from the sizing field
-for i in range(3):  # ellipse around the selected node
-    plot_ellipse(
-        ax2, sizing_field2[containing_tris[idx][i]],
-        mesh2["mesh_pos"][BATCH][containing_tris[idx][i]].unbind(), "c"
-    )
-for i in np.arange(len(normal1_mask))[delete_nodes]:    # ellipse for deleted nodes
-    S = S_i[i]
-    plot_ellipse(
-        ax2, S,
-        mesh1["mesh_pos"][BATCH][i].unbind(), "r"
-    )
-"""
 
 fig.tight_layout()
 plt.show()
